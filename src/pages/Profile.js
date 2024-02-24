ed1bbe0011c455ce0e602f1818bbb8205e340756
Cheryl Lim <cheryllim@g.ucla.edu>
2024-02-24 18:36:12 -0500

    Moved Profile to right of NavBar

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -11,10 +11,3 @@ export default function Profile() {
   );
 }
 
-// Navigation bar link with profile picture
-export const ProfileLink = () => (
-  <Link to="/Profile" className="profile-link">
-    <img src="/images/profile.png" alt="Profile" className="profile-picture" />
-    Profile
-  </Link>
-);
