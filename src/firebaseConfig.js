2b07227d24ff5802b653756d61f6e14a4358b138
justbustin <justinnguyen80@gmail.com>
2024-02-27 17:02:36 -0800

    connected firebase to workout plans

---
+++ src/firebaseConfig.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+import 'firebase/compat/analytics';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAYxjvnG_EtV5OJ2YZKsu0NuOtBhmyY_uQ",
+    authDomain: "gigab-d299f.firebaseapp.com",
+    projectId: "gigab-d299f",
+    storageBucket: "gigab-d299f.appspot.com",
+    messagingSenderId: "67991910335",
+    appId: "1:67991910335:web:856b643435b1aa5340d30a",
+    measurementId: "G-0G9GXYGCE1"
+}
+
+firebase.initializeApp(firebaseConfig)
+  
+  const auth = firebase.auth();
+  const db = firebase.firestore();
+
+export {db, auth}
