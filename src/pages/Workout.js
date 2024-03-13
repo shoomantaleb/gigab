aa76ea1177925237691651887b29c6ff0c4bb0aa
Zach Fischer <zfischer42@gmail.com>
2024-03-13 09:48:13 -0700

    Display workout titles on hover

--- src/pages/Workout.js
+++ src/pages/Workout.js
@@ -260,7 +260,7 @@ export default function Workout() {
           <div className="box">
             {displayExercise ? (
               <div className="exercise-info">
-                <h1 className="exercise-info-title">{displayExercise.title}</h1>
+                <h1 className="exercise-info-title">{displayExercise.name}</h1>
                 <h2 className="exercise-info-category">{displayExercise.category}</h2>
                 <img src={displayExercise.imageUrl} 
                   style={{width:"40%", marginLeft:"auto", marginRight:"auto", borderRadius:"30px", boxShadow:"0 2px 5px grey"}} ></img>
