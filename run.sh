b6efc0d7470397a86abfb264ea672ca502a97380
Zach Fischer <zfischer42@gmail.com>
2024-03-13 23:13:20 -0700

    Only ask for api key if it has not been replaced

--- run.sh
+++ run.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-echo "Please input API key:"
-read apiKey
 
-# Cross-platform compatibility for in-place sed editing
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  # macOS requires a backup extension with -i, even if it's just an empty string ('')
-  sed -i '' "s|REPLACE_API_KEY|$apiKey|g" ./src/firebaseConfig.js
-else
-  # Linux allows -i without an extension to edit in-place without backup
-  sed -i "s|REPLACE_API_KEY|$apiKey|g" ./src/firebaseConfig.js
+if grep -q "REPLACE_API_KEY" "./src/firebaseConfig.js"; then
+    echo "Please input API key:"
+    read apiKey
+    # Cross-platform compatibility for in-place sed editing
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        # macOS requires a backup extension with -i, even if it's just an empty string ('')
+        sed -i '' "s|REPLACE_API_KEY|$apiKey|g" ./src/firebaseConfig.js
+    else
+        # Linux allows -i without an extension to edit in-place without backup
+        sed -i "s|REPLACE_API_KEY|$apiKey|g" ./src/firebaseConfig.js
+    fi
 fi
 
 npm install
