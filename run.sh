5591e1e9bc08505f9f090c658cc3439688bc1029
justbustin <justinnguyen80@gmail.com>
2024-03-13 22:37:30 -0700

    fixed run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-echo "Please input API key"
-read $apiKey
+echo "Please input API key:"
+read apiKey
 
-sed -i "s/REPLACE_API_KEY/$apiKey/g" firebaseConfig.js
+# Cross-platform compatibility for in-place sed editing
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # macOS requires a backup extension with -i, even if it's just an empty string ('')
+  sed -i '' "s|REPLACE_API_KEY|$apiKey|g" ./src/firebaseConfig.js
+else
+  # Linux allows -i without an extension to edit in-place without backup
+  sed -i "s|REPLACE_API_KEY|$apiKey|g" ./src/firebaseConfig.js
+fi
 
 npm install
 npm start
